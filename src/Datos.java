98be0a94723de8659611d0e6eec9e17f28adecc4
Beru748 <ericksamirwaton@gmail.com>
2026-04-15 09:16:35 -0500

    refactor:mejora en el formato del codigo

--- src/Datos.java
+++ src/Datos.java
@@ -3,17 +3,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-
 /**
  *
  * @author alfre
  */
 public class Datos {
-public static String[] nom={"Bandeja Paisa","Sancocho de Gallina","Arepa con Huevo","Jugo Natural","Gaseosa","Cerveza Poker","Agua Panela","Arroz con Pollo"};
-public static double[] p={32000,28000,8000,7000,4500,6000,3500,25000};
-public static int[] cant={0,0,0,0,0,0,0,0};
-public static int ms=0;public static int est=0;
-public static double tot=0;public static int nf=1;
-public static String x="El Buen Sabor";
-public static String tmp="";    
+
+    public final static String NOMBRE_RESTAURANTE = "RESTAURANTE EL BUEN SABOR";
+    public final static String DIRECCION = "Calle 15 #8-32, Valledupar";
+    public final static String NIT = "NIT: 900.123.456-7";
+
+    
+    public static String[] nombresPlatos = { "Bandeja Paisa", "Sancocho de Gallina", "Arepa con Huevo", "Jugo Natural", "Gaseosa",
+            "Cerveza Poker", "Agua Panela", "Arroz con Pollo" };
+    public static double[] p = { 32000, 28000, 8000, 7000, 4500, 6000, 3500, 25000 };
+    public static int[] cant = { 0, 0, 0, 0, 0, 0, 0, 0 };
+    public static int ms = 0;
+    public static int est = 0;
+    public static double tot = 0;
+    public static int nf = 1;
+    public static String x = "El Buen Sabor";
+    public static String tmp = "";
 }
---
+++ src/Producto.java
@@ -0,0 +1,42 @@
+public class Producto {
+    private String nombre;
+    private String precio;
+    private String cantidad;
+    
+    public Producto(String nombre, String precio, String cantidad) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(String cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto [nombre=" + nombre + ", precio=" + precio + ", cantidad=" + cantidad + "]";
+    }
+
+    
+}
